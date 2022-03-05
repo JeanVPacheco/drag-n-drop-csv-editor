99c06fb4780e71c8c87498e1cbf508b513ca1bd0
JeanVPacheco <jeanvitorp@gmail.com>
2022-03-05 13:50:57 -0300

    Populating table with CSV info

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -2,9 +2,54 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Table({ info }) {
+  if (!info[0]) {
+    return null;
+  }
   console.log(info);
+  const headerValues = Object.keys(info[0]);
+  console.log('headerValues', headerValues);
+
+  const tableHead = (
+    <thead>
+      <tr>
+        {
+          headerValues.map((value) => <th key={value}>{ value }</th>)
+        }
+        <th>
+          Excluir
+        </th>
+      </tr>
+    </thead>
+  );
+
+  const tableBody = (
+    <tbody>
+      {
+        info.map((entry) => (
+          <tr key={entry.id}>
+            {
+              Object.values(entry).map((attribute) => (
+                <td key={attribute}>{attribute}</td>
+              ))
+            }
+            <td>
+              <button
+                type="button"
+              >
+                Remover
+              </button>
+            </td>
+          </tr>
+        ))
+      }
+    </tbody>
+  );
+
   return (
-    <p>Tabela</p>
+    <table>
+      {tableHead}
+      {tableBody}
+    </table>
   );
 }
 
